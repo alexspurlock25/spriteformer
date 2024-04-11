fd0e3d406edc22450fe6a142c454ddf94f4f387e
Alexander Spurlock <alexanderspurlock@gmail.com>
2024-04-11 16:56:10 -0400

    removing some stuff

--- main.js
+++ main.js
@@ -8,21 +8,36 @@ const size = {
 
 const gravity = 100
 
+const ASSET_KEYS = {
+  BACKGROUND: "background",
+  PLAYER: {
+    IDLE: "player_idle",
+    RUN: "player_run",
+    JUMP: "player_jump"
+  }
+}
+
 class GameScene extends Phaser.Scene {
   constructor() {
     super("game-scene");
   }
 
   // prelaod assets here
-  preload() {}
+  preload() {
+    this.load.image(ASSET_KEYS.BACKGROUND, "/assets/background.png")
+    this.load.image(ASSET_KEYS.PLAYER.JUMP, "/assets/player/jump.png")
+  }
 
   // handle the preloaded assets here
   create() {
-    this.add.text(
-      size.width / 2 - 70, 
-      size.height / 2, 
-      "Hello, World!"
-    );
+    this.add.tileSprite(0, 0, config.width, config.height, ASSET_KEYS.BACKGROUND)
+    this.add.image(0, 0, ASSET_KEYS.PLAYER.JUMP)
+    // this.add.image(0, 0, ASSET_KEYS.BACKGROUND)
+    // this.add.text(
+    //   size.width / 2 - 70, 
+    //   size.height / 2, 
+    //   "Hello, World!"
+    // );
   }
     
     // runs on every frame
