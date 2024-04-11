b9ab06638dd074c7b2720dad874eba2e8a803ba0
Alexander Spurlock <alexanderspurlock@gmail.com>
2024-04-11 09:32:45 -0400

    basic game config

--- main.js
+++ main.js
@@ -2,44 +2,46 @@ import './style.css'
 import Phaser from "phaser";
 
 const size = {
-    width: 1000,
-    height: 700,
+  width: 1000,
+  height: 700,
 };
 
+const gravity = 100
+
 class GameScene extends Phaser.Scene {
-    constructor() {
-      super("game-scene");
-    }
-  
-    preload() { 
-        
-    }
-  
-    create() {
-        this.add.text(
-            size.width / 2 - 70, 
-            size.height / 2, 
-            "Hello, World!"
-        );
-     }
-  
-    update() {
-        
-     }
+  constructor() {
+    super("game-scene");
+  }
+
+  // prelaod assets here
+  preload() {}
+
+  // handle the preloaded assets here
+  create() {
+    this.add.text(
+      size.width / 2 - 70, 
+      size.height / 2, 
+      "Hello, World!"
+    );
+  }
+    
+    // runs on every frame
+    update() { }
 }
 
 const config = {
-    type: Phaser.WEBGL,
-    width: size.width,
-    height: size.height,
-    canvas: gameCanvas,
-    physics: {
-      default: "arcade",
-      arcade: {
-        gravity: { y: 1 },
-      },
+  type: Phaser.WEBGL,
+  width: size.width,
+  height: size.height,
+  canvas: gameCanvas,
+  physics: {
+    default: "arcade",
+    arcade: {
+      gravity: { y: gravity },
+      debug: true
     },
-    scene: [GameScene],
+  },
+  scene: [GameScene],
 };
   
 const game = new Phaser.Game(config);
