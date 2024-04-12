e126c724f48f361fe342cf25003393eba0d79a28
Alexander Spurlock <alexanderspurlock@gmail.com>
2024-04-12 10:34:56 -0400

    code reorg

--- main.js
+++ main.js
@@ -12,9 +12,11 @@ const ASSET_KEYS = {
   BACKGROUND: "background",
   PLATFORMS: "platforms",
   PLAYER: {
+    FALL: "player_fall",
+    HIT: "player_hit",
     IDLE: "player_idle",
-    RUN: "player_run",
-    JUMP: "player_jump"
+    JUMP: "player_jump",
+    RUN: "player_run"
   }
 }
 
@@ -27,7 +29,14 @@ class GameScene extends Phaser.Scene {
   preload() {
     this.load.image(ASSET_KEYS.BACKGROUND, "/assets/background.png")
     this.load.image(ASSET_KEYS.PLAYER.JUMP, "/assets/player/jump.png")
+    
+    // player
+    this.load.image(ASSET_KEYS.PLAYER.FALL, "/assets/player/fall.png")
+    this.load.spritesheet(ASSET_KEYS.PLAYER.HIT, "/assets/player/hit.png", { frameWidth: 32, frameHeight: 32 })
     this.load.spritesheet(ASSET_KEYS.PLAYER.IDLE, "/assets/player/idle.png", { frameWidth: 32, frameHeight: 32 })
+    this.load.image(ASSET_KEYS.PLAYER.JUMP, "/assets/player/jump.png")
+    this.load.spritesheet(ASSET_KEYS.PLAYER.RUN, "/assets/player/run.png", { frameWidth: 32, frameHeight: 32 })
+
     this.load.spritesheet(ASSET_KEYS.PLATFORMS, "/assets/terrain.png", { frameWidth: 48, frameHeight: 48 })
   }
 
@@ -36,10 +45,14 @@ class GameScene extends Phaser.Scene {
     this.add.tileSprite(0, 0, config.width, config.height, ASSET_KEYS.BACKGROUND)
       .setOrigin(0, 0)
 
-      for (let i = 24; i < config.width; i = i + 90) {
-        this.add.image(i, config.height - 50, ASSET_KEYS.PLATFORMS, 2)        
-      }
+    this.createLevel()
+    this.createPlayer()
+  }
+    
+    // runs on every frame
+    update() { }
 
+    createPlayer() {
       this.anims.create({
         key: "idle",
         frameRate: 24,
@@ -47,12 +60,15 @@ class GameScene extends Phaser.Scene {
         repeat: -1
       });
     
-    this.player = this.add.sprite(24, config.height - 90, ASSET_KEYS.PLAYER.IDLE)
-    this.player.anims.play("idle")
-  }
-    
-    // runs on every frame
-    update() { }
+      this.player = this.add.sprite(24, config.height - 90, ASSET_KEYS.PLAYER.IDLE)
+      this.player.anims.play("idle")
+    }
+
+    createLevel() {
+      for (let i = 24; i < config.width; i = i + 90) {
+        this.add.image(i, config.height - 50, ASSET_KEYS.PLATFORMS, 2)        
+      }
+    }
 }
 
 const config = {
